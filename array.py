40fe24c66ad32db499ba4d5afd30ff48e1b1e5f9
Agustina Kelly <agustinagkelly@gmail.com>
2023-02-03 16:21:41 -0300

    archivo nuevo

---
+++ array.py
@@ -0,0 +1,16 @@
+# Forma LARGA
+#declarar array
+miArray = []
+
+#inicalizar array
+miArray.append(1)
+miArray.append('Luis')
+miArray.append(3)
+miArray.append(4)
+
+#mostrar array
+print(miArray)
+
+# Forma CORTA
+miArray2 = ['array2',1,'dos',33]
+print(miArray2)
